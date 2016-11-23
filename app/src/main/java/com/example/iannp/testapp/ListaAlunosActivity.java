575ded7e72fccbea6c5fdeb2e8ea92a2cd2f7620
Ian Pedro <ian0198@gmail.com>
2016-11-23 14:49:21 -0200

    New contextMenu now created to give actions to perform in each student; deleteStudent() method now created in AlunoDAO class.

--- app/src/main/java/com/example/iannp/testapp/ListaAlunosActivity.java
+++ app/src/main/java/com/example/iannp/testapp/ListaAlunosActivity.java
@@ -3,10 +3,14 @@ package com.example.iannp.testapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.iannp.testapp.DAO.AlunoDAO;
 import com.example.iannp.testapp.model.Aluno;
@@ -15,6 +19,8 @@ import java.util.List;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
+
+    private ListView listaAlunos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +34,38 @@ public class ListaAlunosActivity extends AppCompatActivity {
                 startActivity(vaiProForm);
             }
         });
+         listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+
+        registerForContextMenu(listaAlunos);
+
     }
 
     private void loadList() {
         AlunoDAO dao = new AlunoDAO(this);
         List<Aluno> alunos = dao.buscaAlunos();
         dao.close();
-        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
         ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem delete = menu.add("delete");
+        delete.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+                Toast.makeText(ListaAlunosActivity.this, "Aluno" + aluno.getNome() + " deletado!", Toast.LENGTH_SHORT).show();
+                AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
+                dao.deleta(aluno);
+                dao.close();
+                loadList();
+                return false;
+            }
+        });
+
     }
 
 
